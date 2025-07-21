7df3a95df74cf9c59ce1c9e276b306a7724e8020
svtzoneph <accstream92@gmail.com>
2025-07-21 16:26:35 +0800

    Update and rename sw.js to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'zonevault-v2'; // ⬅ Change this on every update
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './home.html',
+  './nanabnb.html',
+  './hxwfanconcert.html',
+  './svtholiday.html',
+  './arenatour.html',
+  './svtjapanconcert.html'
+];
+
+// Install: cache the new version
+self.addEventListener('install', event => {
+  self.skipWaiting(); // Force the new SW to activate immediately
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate: remove old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch: respond from cache or network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
