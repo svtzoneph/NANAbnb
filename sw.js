5dce804670388b774e3b37ca5e916cf31751d10e
svtzoneph <accstream92@gmail.com>
2025-11-19 22:35:49 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 🔹 Version your cache
-const CACHE_NAME = 'zonevault-v102'; // increment version when you update cache
+const CACHE_NAME = 'zonevault-v103'; // increment version when you update cache
 const urlsToCache = [
   './',
   './index.html',
