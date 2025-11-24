d784bb82586dc895453a895e4ce0091419148bf6
svtzoneph <accstream92@gmail.com>
2025-11-24 18:25:56 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -39,7 +39,7 @@ messaging.onBackgroundMessage((payload) => {
 // ============================
 // 2. CACHING
 // ============================
-const CACHE_NAME = 'zonevault-v115'; // UPDATED VERSION
+const CACHE_NAME = 'zonevault-v116'; // UPDATED VERSION
 const urlsToCache = [
   './',
   './index.html',
